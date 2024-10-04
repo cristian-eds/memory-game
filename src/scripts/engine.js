cbf2268d9ee8ed2a32a679207b398c184c6b3370
Cristian <cristian.santos0308@gmail.com>
2024-10-04 10:27:48 -0300

    Adicionado emojis e funcao para sortear emojis e adicionar emoji no game

---
+++ src/scripts/engine.js
@@ -0,0 +1,26 @@
+const emojis = [
+    "💵",
+    "💵",
+    "💰",
+    "💰",
+    "👽",
+    "👽",
+    "🤖",
+    "🤖",
+    "👻",
+    "👻",
+    "🤡",
+    "🤡"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() =>
+    (Math.random() > 0.5 ? 2 : -1));
+
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
